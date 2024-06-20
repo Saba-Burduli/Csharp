2ed51ca0c38a46c8e2e900a2d9cf311ba52b45c4
Comic Solvency <131397277+ComicBurduli@users.noreply.github.com>
2024-06-20 21:38:04 +0400

    homework1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+              int[] familyyears = { 0, 10, 15, 23, 45, 45, 60, 70 };
+
+            
+
+
+
+              int[] axalgazrdebi = { };
+              int[] suaxnis = { };
+              int[] mopxucebi = { };
+
+           
+
+
+            for (int i = 0; i < familyyears.Length; i++) {
+
+                if (familyyears[i] == 0) {
+                    Console.WriteLine(axalgazrdebi += familyyears[]);
+                   
+                
+               }    
+                    
+            }
+
+            
+
+
+            // Example: Convert an array of integers to an array of doubles
+
+
+
+        }
+    }
+}
