644946e55a576aa3f94e02258c6d6f75a7f4f3b2
Comic Solvency <131397277+ComicBurduli@users.noreply.github.com>
2024-06-20 21:40:27 +0400

    homework4

---
+++ projtest/projtest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projtest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] ints = { 1, 2, 3 };   
+
+
+            for (int i = 0; i < ints.Length; i++) {
+            
+            if (ints[i] == ints.Length) {
+
+                    Console.WriteLine(ints[i]); 
+                
+                }
+            
+            }
+           
+        }
+    }
+}
