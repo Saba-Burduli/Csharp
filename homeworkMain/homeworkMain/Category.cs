3d3fc816b9d9c499b0b9bc14cda62e4aea582b9d
Comic Solvency <131397277+ComicBurduli@users.noreply.github.com>
2024-06-20 21:49:18 +0400

    homework3

---
+++ homeworkMain/homeworkMain/Category.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace homeworkMain
+{
+    internal class Category : Product
+    {
+
+
+        public string ManufacturerName { get; set; } //mwarmoebelis saxeli
+
+        public string CategoryName { get; set; }  //kategoris saxeli
+
+
+
+
+
+        public override string PrintMethod()
+        {
+
+
+
+
+            return base.PrintMethod() + $" ManufacturerName : {ManufacturerName}  -   CategoryName : {CategoryName}   ";
+        }
+
+
+
+
+        public override void search()
+        {
+            base.search();
+            Console.WriteLine("search category and ProductName :");
+            Category = Console.ReadLine();
+            ProductName = Console.ReadLine();  
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
